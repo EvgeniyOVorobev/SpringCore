4c1f10fedf4239a4d6e8d6f0288a0419ef96837c
EvgeniyOVorobev <vorobiev71@yandex.ru>
2023-03-14 20:26:42 +0300

    + init, destroy and factory method

--- src/main/java/com/ev/ClassicalMusic.java
+++ src/main/java/com/ev/ClassicalMusic.java
@@ -1,7 +1,19 @@
 package com.ev;
 
 public class ClassicalMusic implements Music{
+    private ClassicalMusic(){
+
+    }
+    public static ClassicalMusic getClassicalMusic(){
+        return new ClassicalMusic();
+    }
     public String getSong() {
         return "Hungarian Rhapsody";
+    }
+    public void doMyInit(){
+        System.out.println("Doing my initialization");
+    }
+    public void doMyDestroy(){
+        System.out.println("doing my distraction");
     }
 }
--- src/main/java/com/ev/TestSpring.java
+++ src/main/java/com/ev/TestSpring.java
@@ -5,12 +5,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("ApplicationContext.xml");
-       MusicPlayer music= context.getBean("musicPlayer", MusicPlayer.class) ;
-       MusicPlayer musicPlayer=context.getBean("musicPlayer", MusicPlayer.class);
-       boolean comparison=musicPlayer==music;
-        System.out.println(comparison);
-        music.playMusic();
-        System.out.println(music.getName()+" "+music.getValue());
+//       MusicPlayer music= context.getBean("musicPlayer", MusicPlayer.class) ;
+//       MusicPlayer musicPlayer=context.getBean("musicPlayer", MusicPlayer.class);
+//       boolean comparison=musicPlayer==music;
+//        System.out.println(comparison);
+//        music.playMusic();
+//        System.out.println(music.getName()+" "+music.getValue());
+        ClassicalMusic classicalMusic=context.getBean("musicBean1", ClassicalMusic.class);
+        ClassicalMusic classicalMusic1=context.getBean("musicBean1", ClassicalMusic.class);
+
        context.close();
     }
 }
