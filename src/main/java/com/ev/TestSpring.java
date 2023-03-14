3cf633fc99f58c346d76ce0fb67993528b087309
EvgeniyOVorobev <vorobiev71@yandex.ru>
2023-03-14 19:17:34 +0300

    + scope prototype;

--- src/main/java/com/ev/TestSpring.java
+++ src/main/java/com/ev/TestSpring.java
@@ -6,7 +6,9 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context=new ClassPathXmlApplicationContext("ApplicationContext.xml");
        MusicPlayer music= context.getBean("musicPlayer", MusicPlayer.class) ;
-
+       MusicPlayer musicPlayer=context.getBean("musicPlayer", MusicPlayer.class);
+       boolean comparison=musicPlayer==music;
+        System.out.println(comparison);
         music.playMusic();
         System.out.println(music.getName()+" "+music.getValue());
        context.close();
